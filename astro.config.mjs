62a857753e27188ffb8c45b4093b63249915cec3
UFEQ1337 <ufeq@icloud.com>
2026-06-26 11:17:18 +0200

    Zaktualizowano konfigurację Astro, usunięto nieużywane zależności fontów, dodano nowe czcionki oraz poprawiono komponenty, aby obsługiwały natywne karuzele i responsywne obrazy.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zaskoki.pl', // docelowa domena (kanoniczne URL-e, Open Graph, sitemapa)
-  output: 'static',
+  site: "https://zaskoki.pl", // docelowa domena (kanoniczne URL-e, Open Graph, sitemapa)
+  output: "static",
   integrations: [sitemap()],
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: "always",
   },
   image: {
     responsiveStyles: true,
